6d7263051f6dacdb6e2a62e315e5d91e1d287c73
Milacm35 <126989090+Milacm35@users.noreply.github.com>
2023-03-18 17:04:07 -0500

    Cuarta carga

---
+++ HTML-main/js/script.js
@@ -0,0 +1,9 @@
+
+
+function sumar(){
+	console.log("Hola");
+	let	num1 = document.getElementById("num1").value;
+	let	num2 = document.getElementById("num2").value;
+	let suma = parseInt(num1) + parseInt(num2)
+	console.log("El resultado de la suma es:" + suma)
+}
